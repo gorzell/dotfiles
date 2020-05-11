17a3a6152f98f6ffedbb452682f91dc8d1564d00
Greg Orzell <gorzell@users.noreply.github.com>
2020-05-11 14:27:05 +0000

    Make the install script non-root proof.

--- install.sh
+++ install.sh
@@ -1,9 +1,15 @@
-#!/bin/sh
-
+#!/bin/bash
 # Install script for GitHub Codespaces
 
-apt-get update
+set -ex
 
-apt-get install -y --no-install-recommends ca-certificates curl fish git make openssh-client tmux vim
+apt="apt-get"
+
+if (( $EUID != 0 )); then
+    apt="sudo $apt"
+fi
+
+$apt update
+$apt install -y --no-install-recommends ca-certificates curl fish git make openssh-client tmux vim
 
 make bootstrap install
