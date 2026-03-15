8e5ca1af12a7f4a96041efaad62016413dc26f50
Greg Orzell <gorzell@github.com>
2026-03-15 12:26:14 +0100

    Updates.

--- fish/config.fish
+++ fish/config.fish
@@ -37,3 +37,7 @@ end
 if test -d ~/.asdf
     source ~/.asdf/asdf.fish
 end
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
